3d0f950d36c7b95be6099fd3072e66acb801cb63
DIR\sattam.rakshit <sattam.rakshit@acp.aws.accenture.com>
2017-03-25 10:49:19 +0530

    Changes getting pushed to repo

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+//Initialize our express web framework
 var express = require('express');
+
 var app = express();
+//The port number where we are going to listen
 var port = '8080';
+// Native Nodejs module for resolving paths
 var path = require('path');
+// Set our View Engine to EJS and set the directory to where our views will be present
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'client', 'views'));
+//Set the path to load the static js files
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+//Set our entry point after the webpage loads
 app.get('/webapp', function(req, res) {
 	res.render('index.ejs');
 });
-
+// The port where we will listen
 app.listen(port, function() {
 	console.log('Server Running...' + port);
 });
